d616e5c7e2b8f2434fc6e9aad9498d666e0f1b22
Torsten Link <torsten.link@db.com>
2016-02-23 23:13:40 +0100

    WIP

--- tests/tdg_run.js
+++ tests/tdg_run.js
@@ -3,8 +3,10 @@
 
 "use strict";
 
-const tdgExecuter = require('../lib/tdg-executer');
-const tdgPreprocessor = require('../lib/tdg-preprocessor');
+const model = require('../lib/model-tdg').factory();
+const executer = require('../lib/tdg-executer').factory();
+const writerFactory = require('../lib/tdg-writer').factory;
+
 
 const fs = require('fs');
 const path = require("path");
@@ -15,15 +17,86 @@ const volatileDir = path.join(__dirname, 'fixtures', 'volatile');
 
 
 //const structureContent = fs.readFileSync(path.join(fixturesDir, 'test_structure.json'));
-const structureContent = fs.readFileSync(path.join(fixturesDir, 'new_definition.json'));
+const structureContent = fs.readFileSync(path.join(fixturesDir, 'model_small.json'));
+//const structureContent = fs.readFileSync(path.join(fixturesDir, 'model_small-highValue.json'));
 const structureJson = JSON.parse(structureContent);
 
-if (tdgPreprocessor.loadStructure(structureJson, volatileDir, false)) {
-	// The file could be successfully be processed. Now start generating data
+console.log("# Start model parsing");
+model.init(structureJson);
 
-	// reload the precpressed JSON
-	const structureContent = fs.readFileSync(path.join(volatileDir, 'prePrcessedGraphDefinition.json'));
-	const structureJson = JSON.parse(structureContent);
+console.log("# Start executer");
 
-	//tdgExecuter.loadStructure(structureJson, volatileDir);
+// Set the model to the executer
+const writer = writerFactory({
+	"targetDir": volatileDir
+});
+executer.model = model;
+executer.writer = writer;
+
+createMethods(executer);
+
+executer.createVertices();
+executer.createEdges();
+
+
+/**
+ * Add functions to the TDG-Executer
+ */
+function createMethods(tdgExecuter) {
+	// Maps the accounts to its parent
+	const idMapper = {};
+
+
+
+	tdgExecuter.getSourceApplicationHasAccount = function (edgeConfig) {
+		const name = 'application_has_account';
+		const sourceEdge = this.registry[name];
+		const resArray = [];
+
+		Object.keys(sourceEdge.objects).forEach(appId => {
+			sourceEdge.objects[appId].forEach(val => {
+				resArray.push(val);
+				idMapper[val] = appId;
+			});
+		});
+
+		return resArray;
+	};
+
+	tdgExecuter.getTargetApplicationHasEntitlement = function (edgeConfig, accountId) {
+		const name = 'application_has_entitlement';
+
+		const sourceEdge = this.registry[name];
+		const appId = idMapper[accountId];
+
+		//		console.log(`appId = ${appId} for accountId = ${accountId}`);
+
+		const resArray = sourceEdge.objects[appId];
+		return resArray;
+
+		// const resMap = new Map();
+		// let keyCounter = 0;
+		// resArray.forEach(val => {
+		// 	resMap.set(keyCounter, val);
+		// 	keyCounter++;
+		// });
+		//
+		// return resMap;
+	};
+
+	tdgExecuter.getSourceLengthApplicationHasEntitlement = function (edgeConfig, accountId) {
+		const name = 'application_has_entitlement';
+		const sourceEdge = this.registry[name];
+
+		const appId = idMapper[accountId];
+
+		const resNumber = sourceEdge.objects[appId].length;
+
+		//		console.log(`Get ${resNumber} possible targets for app ${appId}`);
+
+		return resNumber;
+	};
+
+
+
 }
