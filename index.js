283594c5381e9c1da1a6dcdc2f338a0bdcba5622
Torsten Link <torsten.link@db.com>
2016-03-16 10:26:07 +0100

    feat(all): Refactor and add a comen main script to start each step.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 /* jslint node: true, esnext: true */
 "use strict";
 
-const TdgModel = require('./lib/tdg-model').factory;
-const TdgExecuter = require('./lib/tdg-executer').factory;
-const TdgExecuterData = require('./lib/tdg-executer-data').factory;
-const TdgWriter = require('./lib/tdg-writer').factory;
-const DataGenerator = require('./lib/data-generator').factory;
-const TdgFileExporter = require('./lib/tdg-file-exporter').factory;
+// const TdgModel = require('./lib/tdg-model').factory;
+// const TdgExecuter = require('./lib/tdg-executer').factory;
+const main = require('./lib/main');
 
-module.exports.TdgModel = TdgModel;
-module.exports.TdgExecuter = TdgExecuter;
-module.exports.TdgExecuterData = TdgExecuterData;
-module.exports.TdgWriter = TdgWriter;
-module.exports.DataGenerator = DataGenerator;
-module.exports.TdgFileExporter = TdgFileExporter;
+
+
+// const TdgExecuterData = require('./lib/tdg-executer-data').factory;
+// const TdgWriter = require('./lib/tdg-writer').factory;
+// const DataGenerator = require('./lib/data-generator').factory;
+// const TdgFileExporter = require('./lib/tdg-file-exporter').factory;
+//
+// module.exports.TdgModel = TdgModel;
+// module.exports.TdgExecuter = TdgExecuter;
+// module.exports.TdgExecuterData = TdgExecuterData;
+// module.exports.TdgWriter = TdgWriter;
+// module.exports.DataGenerator = DataGenerator;
+// module.exports.TdgFileExporter = TdgFileExporter;
+module.exports.main = main;
