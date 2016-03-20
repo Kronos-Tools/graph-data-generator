f58554d3d4999af14052791eef54c78ec2dea707
Torsten Link <torsten.link@db.com>
2016-03-20 15:47:14 +0100

    WIP

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 /* jslint node: true, esnext: true */
 "use strict";
 
-
-// Normaly you should only need this module
-// const main = require('./lib/main');
-// module.exports.main = main;
-
-const main2 = require('./lib/main2');
-module.exports.main2 = main2;
-
+module.exports.main = require('./lib/main');
 module.exports.tdg = require('./lib/exports.js');
