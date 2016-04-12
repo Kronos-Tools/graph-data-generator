ece40b206b264d865130eccf5e97ed56be026911
Torsten Link <torsten.link@db.com>
2016-04-12 22:35:41 +0200

    feat(travis): Checkin to trigger travis

---
+++ tests/dummy_test.js
@@ -0,0 +1,20 @@
+/* global describe, it, beforeEach */
+/* jslint node: true, esnext: true */
+"use strict";
+
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+const fs = require('fs');
+const path = require("path");
+const rimraf = require('rimraf');
+
+
+describe('dummy', function () {
+
+	it('dummy test', function (done) {
+		done();
+	});
+});
